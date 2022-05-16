1ec482a473a7f3842fab5e83b748583411341338
airKing05 <anil050797@gmail.com>
2022-05-16 14:07:54 +0530

    db, fetch restaunrant

--- react-food/src/components/ProductList.js
+++ react-food/src/components/ProductList.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Table } from "react-bootstrap";
 
 export default function ProductList() {
+    const [restaurant, setRestaurent] = useState([]);
+
+    function getRestaurant(){
+        fetch('http://localhost:3001/restaurant')
+        .then(res => res.json())
+        .then(result => {
+            console.table(result)
+            setRestaurent(result)
+        } )
+    };
+    useEffect(()=>{
+        getRestaurant();
+    }, [])
+   
+
   return (
-    <div>this is product listing page thkjsdbvkjsdbvs</div>
+    <div>
+          <Table striped bordered hover>
+              <thead>
+                  <tr>
+                      <th>Sr. No.</th>
+                      <th>Restaurant</th>
+                      <th>Address</th>
+                      <th>Email</th>
+                      <th>Rating</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {restaurant && restaurant.map((item, index) =>
+                  <tr key={index}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.address}</td>
+                      <td>{item.email}</td>
+                      <td>{item.rating}</td>
+                  </tr> )}
+              </tbody>
+          </Table>
+       
+    </div>
   )
 }
